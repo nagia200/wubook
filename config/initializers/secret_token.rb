27efad38f472854bda2e7c5ee846ea93a9188f40
Nagi Argiriou <nagiargiriou@Nagi-Argirious-MacBook-Pro.local>
2014-05-01 18:35:24 -0400

    Created a rails app and added statuses.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wubook::Application.config.secret_key_base = 'ca1622e8a038d5605ea8ef5f98de243d9a84872d46b21d00e474867e271978252079cb9de3d01c1ff5c90aa0016d5fba80748f68aa643c91c24d1f5f2f9c9b14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wubook::Application.config.session_store :cookie_store, key: '_wubook_session'
